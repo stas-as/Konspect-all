72fc3e184ebcbed8ab1129129c2b42f823e9678d
stas-as <124561396+stas-as@users.noreply.github.com>
2024-04-25 21:00:10 +0300

    1

--- main.py
+++ main.py
@@ -1,14 +1,26 @@
-from fastapi import FastAPI
+from fastapi import *
 from pydantic import BaseModel
 from typing import Optional
+from typing import Annotated
 app = FastAPI()
 
-class Task(BaseModel):
+    
+class StaskAdd(BaseModel):
     name: str
     description: Optional[str] = None
+    
+class STask(StaskAdd):
+    id : int
 
-@app.get("/tasks")
-def get_tasks():
-    task = Task(name="Loading this video")
-    return {"data": task}
+tasks = []
+@app.post("/tasks")
+async def add_task(task: Annotated[StaskAdd, Depends()]):
+    tasks.append(task)
+    return{"OK":True}
+
+
+# @app.get("/tasks")
+# def get_tasks():
+#     task = Task(name="Loading this video")
+#     return {"data": task}
 
